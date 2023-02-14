0a12d9cd93e0016cab05913e4a0b064cb6d291c3
anis <anis.siam@hotmail.com>
2023-02-14 14:56:11 +0200

    //14/Fab/2023 -Decision Making

--- src/com/java5/basics/IfStatment.java
+++ src/com/java5/basics/IfStatment.java
@@ -5,7 +5,105 @@ import java.util.Scanner;
 public class IfStatment {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Plz enter x ");
+        System.out.println("Plz enter Line ");
+        String line = scanner.nextLine();
+      /*  if(line.isEmpty()){
+            System.out.println("Line is empty");
+            return;
+        }
+*/
+        if(!line.isEmpty()&& line.contains("https://")){
+            System.out.println("URL");
+        }else if(!line.isEmpty() &&line.contains("@")){
+            System.out.println("Email");
+        }else {
+            System.out.println("not email or URL");
+        }
+        /*System.out.println("Plz enter mark ");
+        int mark = scanner.nextInt();
+
+        if(mark>0 && mark%2==0){
+            System.out.println("Up to 0 and even");
+        }else if(mark>0 && mark%2!=0){
+            System.out.println("Up  to 0 and Odd ");
+        }else {
+            System.out.println("Less than 0 ");
+        }
+*/
+
+
+        /* int mark = scanner.nextInt();
+        if(mark>6 || mark<1){
+            System.out.println("not correct to add " +mark);
+            return;
+        }*/
+
+        /*if(mark>6){
+            System.out.println("not correct to add " +mark);
+            return;
+        }
+
+        if(mark<1){
+            System.out.println("not correct to add " +mark);
+            return;
+        }*/
+
+        /*if (mark == 1) {
+            System.out.println("1");
+        } else if (mark == 2) {
+            System.out.println("2");
+        } else if (mark == 3) {
+            System.out.println("3");
+        } else if (mark == 4 || mark == 5) {
+            System.out.println("4 OR 5");
+        }  else if (mark == 6) {
+            System.out.println("6");
+        }*//*else {
+            System.out.println("not correct to add " + mark);
+        }*/
+
+
+       /* if(mark>100){
+            System.out.println("not valid");
+            return;
+        }
+
+        if(mark>=90){
+            System.out.println("Up to 90");
+        }else if(mark>=80){
+            System.out.println("up to 80");
+        } else if(mark>=70){
+            System.out.println("UP to 70");
+        }else if(mark>=60){
+            System.out.println("UP to 60");
+        }else {
+            System.out.println("Less than 60");
+        }
+
+*/
+        /*System.out.println("Plz enter number ");
+        int number = scanner.nextInt();
+
+        if(number==0){
+            System.out.println("number equal to 0 ");
+            return;
+        }*/
+
+        /*if(number>0 ){
+            if(number%2==0){
+                System.out.println("Up to 0 and Even");
+            }else {
+                System.out.println("Up to 0 and Odd");
+            }
+        }else if(number<0){
+            System.out.println("Less than 0");
+        } else  {
+            System.out.println("number equal to 0 ");
+        }*/
+
+
+
+        /*System.out.println("Plz enter x ");
         int x = scanner.nextInt();
 
         if (x > 0) {
@@ -13,7 +111,7 @@ public class IfStatment {
         }else {
             System.out.println("NO");
         }
-        System.out.println("Done");
+        System.out.println("Done");*/
 
 
     }
